a033a27872f16ce1a69b4f78e23cc005112094eb
momentiris <momentiris@gmail.com>
2019-03-12 16:09:07 +0100

    chore: merge master

--- src/graphQL.ts
+++ src/graphQL.ts
@@ -6,7 +6,6 @@ import { setContext } from 'apollo-link-context'
 import ApolloClient from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { getCookie, removeCookie, redirect } from './utils/helpers'
-console.log(process.env)
 
 const httpLink = new HttpLink({
   uri: process.env.REACT_APP_GRAPHQL_URI,
--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { RouteComponentProps } from '@reach/router'
 
 const Profile: React.FC<RouteComponentProps> = props => (
-  <div>This is the protected profile page!</div>
+  <div>This page requires logged in.</div>
 )
 
 export default Profile

096fff0cd1d2635b259ba00016e0dff0ca65ed49
Andreas Warme <apn@Andreass-MacBook-Pro.local>
2019-03-18 17:09:32 +0100

    build change

--- infrastructure/travis/deploy.sh
+++ infrastructure/travis/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-token=`cat ./infrastructure/travis/openshift-token`
+token=`cat ./infrastructure/travis/openshift-token` --insecure-skip-tls-verify=true
 oc login $OPENSHIFT_URL --token=$token
 oc rollout latest api-ci -n myskills

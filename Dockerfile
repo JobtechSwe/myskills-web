1afb4811dd6559a01d96359ff404b4d288ef3e33
Radu Achim <5114595+raduachim@users.noreply.github.com>
2019-03-19 17:01:59 +0100

    Feat/openshift (#7)
    * ci: 🎡 add openshift files for CI env
    
    * ci: 🎡 add openshift deploy instructions for Travis

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,12 @@ COPY tsconfig.json ./
 COPY public ./public
 COPY src ./src
 
+RUN npm run build
+
+ENV USER=web-user
+RUN adduser --disabled-password --gecos "" $USER
+USER $USER
+
 EXPOSE 4000
 
-CMD ["sh", "-c", "npm run build && npx serve --listen 4000 --no-clipboard --single build"]
+CMD ["sh", "-c", "npx serve --listen 4000 --no-clipboard --single build"]
--- infrastructure/travis/deploy.sh
+++ infrastructure/travis/deploy.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+err=0
+trap 'err=1' ERR
+
 token=`cat ./infrastructure/travis/openshift-token`
 oc login $OPENSHIFT_URL --token=$token --insecure-skip-tls-verify=true
-# TODO: add rollout logic
+if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+  oc rollout latest web-ci -n myskills
+fi
+
+if [[ ! -z "$TRAVIS_TAG" ]]; then
+  oc rollout latest web-test -n myskills
+fi
+
 oc logout
+
+exit $err

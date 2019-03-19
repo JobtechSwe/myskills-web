92fb3a31442233df218907fbc4464d43565c8f0a
Radu Achim <radu.achim@iteam.se>
2019-03-19 10:43:48 +0100

    ci: 🎡 add build stage in Travis config

--- infrastructure/travis/deploy.sh
+++ infrastructure/travis/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 token=`cat ./infrastructure/travis/openshift-token`
 oc login $OPENSHIFT_URL --token=$token --insecure-skip-tls-verify=true
-oc rollout latest api-ci -n myskills
+# TODO: add rollout logic
+oc logout

4ba942d946d1177ec5bfca91496ce11b22f1fa30
Nils Nilsson <nilspontusnilsson@gmail.com>
2019-03-28 21:51:25 +0100

    test: update Button tests

--- src/components/Button/__tests__/Button.spec.tsx
+++ src/components/Button/__tests__/Button.spec.tsx
@@ -4,15 +4,13 @@ import { render } from '../../../utils/test-utils'
 
 describe('components/Button', () => {
   it('renders an Button', async () => {
-    const { container } = render(<Button text="Click me!" />)
+    const { container } = render(<Button>Click me!</Button>)
 
     expect(container).toMatchSnapshot()
   })
 
   it('renders an Button with variant prop', async () => {
-    const { container } = render(
-      <Button text="Click me!" variant="secondary" />
-    )
+    const { container } = render(<Button variant="secondary">Click me!</Button>)
 
     expect(container).toMatchSnapshot()
   })

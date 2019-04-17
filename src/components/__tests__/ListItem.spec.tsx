505fd5b7bdeaf4e5252886ca91e9f81d780cccc0
Nils Nilsson <nilspontusnilsson@gmail.com>
2019-04-17 10:39:25 +0200

    test: added ListItem test

---
+++ src/components/__tests__/ListItem.spec.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import ListItem from '../ListItem'
+import { render } from '../../utils/test-utils'
+
+describe('components/ListItem', () => {
+  it('renders a ListItem', async () => {
+    const { container } = render(<ListItem />)
+
+    expect(container).toMatchSnapshot()
+  })
+})

2f302c50f945e9baeacb5db36f150e49206cb5fa
momentiris <momentiris@gmail.com>
2019-04-24 12:09:14 +0200

    chore: merge master

--- src/views/CreateProfile/MatchSkills.tsx
+++ src/views/CreateProfile/MatchSkills.tsx
@@ -167,7 +167,6 @@ const MatchSkills: React.FC<WithApolloClient<RouteComponentProps>> = ({
     dispatch({ type: 'LOADING', payload: false })
   }
 
-
   useEffect(() => {
     getRelatedSkills(occupations, state.skills)
   }, [occupations])

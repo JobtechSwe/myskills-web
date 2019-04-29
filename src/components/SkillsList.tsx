a4a358fc09671c3ec55b8f5a373c22a7b5a06b1a
Tom Ekander <ekander.tom@gmail.com>
2019-04-29 09:53:56 +0200

    refactor: use Flex for wrapping tags

--- src/components/SkillsList.tsx
+++ src/components/SkillsList.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import Grid from './Grid'
+import Flex from './Flex'
 import Tag from './Tag'
 import { ClientSkillProps } from '../views/CreateProfile/MatchSkills'
-import styled from '@emotion/styled'
-import { OntologyRelationResponse } from '../generated/myskills'
 
 interface SkillsListProps {
   skills: ClientSkillProps[]
@@ -13,20 +11,18 @@ interface SkillsListProps {
 const SkillsList: React.FC<SkillsListProps> = ({ skills, handleAddSkill }) => (
   <>
     föreslagna kompetenser:
-    <Grid
-      gridTemplateColumns="repeat(auto-fit, minmax(150px, 1fr))"
-      style={{ width: '100vw' }}
-    >
+    <Flex justifyContent="center" flexWrap="wrap" style={{ width: '80%' }}>
       {skills.map(skill => (
         <Tag
           key={skill.id}
+          m={5}
           onClick={() => handleAddSkill(skill)}
           variant={skill.isActive ? 'active' : 'default'}
         >
           {skill.term}
         </Tag>
       ))}
-    </Grid>
+    </Flex>
   </>
 )
 

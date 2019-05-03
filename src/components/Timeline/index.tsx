ef8e31fc54d4ef69d928252121016f0d5cb5e812
Nils Nilsson <nilspontusnilsson@gmail.com>
2019-05-03 10:16:50 +0200

    feat: initial work on timeline

---
+++ src/components/Timeline/index.tsx
@@ -0,0 +1,90 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const mockData = [
+  {
+    title: 'Frontendutvecklare',
+    schoolOrCompany: 'Iteam',
+    degree: null,
+    start: '2018/11',
+    end: '2019/04',
+  },
+  {
+    title: 'Frontendutvecklare',
+    schoolOrCompany: 'Webbutveckling',
+    degree: 'Kandidat',
+    start: '2012/01',
+    end: '2018/10',
+  },
+  {
+    title: 'Pistvaktsutbildning',
+    schoolOrCompany: 'Skistar',
+    degree: 'Diplomerad',
+    start: '2009/01',
+    end: '2011/11',
+  },
+]
+
+const Wrapper = styled.div`
+  width: 100%;
+`
+
+const TimelineEntry = styled.div`
+  display: grid;
+  grid-template-columns: 20px 1fr 20px;
+  grid-column-gap: 10px;
+  font-family: ${({ theme }) => theme.fonts.default};
+  margin-bottom: 30px;
+`
+
+const TimelineTitle = styled.div`
+  margin-bottom: 10px;
+`
+
+const TimelineDegree = styled.span`
+  font-size: 12px;
+`
+
+const TimelineInfo = styled.div`
+  align-items: center;
+  display: flex;
+  font-size: 12px;
+`
+
+const TimelineInfoDivider = styled.img`
+  margin: 0 10px;
+`
+
+const TimelineEntryEdit = styled.div`
+  display: flex;
+  align-items: flex-end;
+`
+
+const Timeline: React.FC = () => {
+  return (
+    <Wrapper>
+      {mockData.map((entry, i) => (
+        <TimelineEntry key={`entry-${i}`}>
+          <div>
+            <img src="images/elipse.svg" alt="dot" />
+          </div>
+          <div>
+            <TimelineTitle>
+              {entry.title} <TimelineDegree>, {entry.degree}</TimelineDegree>
+            </TimelineTitle>
+            <TimelineInfo>
+              {entry.schoolOrCompany}
+              <TimelineInfoDivider src="images/dot.svg" alt="divider" />
+              {entry.start} - {entry.end}
+            </TimelineInfo>
+          </div>
+          <TimelineEntryEdit>
+            <img src="images/edit.svg" alt="edit" />
+          </TimelineEntryEdit>
+        </TimelineEntry>
+      ))}
+    </Wrapper>
+  )
+}
+
+export default Timeline
--- src/views/Start/Start.tsx
+++ src/views/Start/Start.tsx
@@ -3,10 +3,12 @@ import Flex from '../../components/Flex'
 import { RouteComponentProps } from '@reach/router'
 import Button from '../../components/Button'
 import { InternalLink } from '../../components/Link'
+import Timeline from '../../components/Timeline'
 
 const Start: React.FC<RouteComponentProps> = () => {
   return (
     <Flex alignItems="center" flexDirection="column" justifyContent="center">
+      <Timeline />
       <Flex p={32}>
         <InternalLink to="/skapa-cv">
           <Button variant="primary">Skapa Cv</Button>

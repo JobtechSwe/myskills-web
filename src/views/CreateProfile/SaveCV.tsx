6fcbc68d5766946314b0fd7fb2f8ec05a27a03ca
Mikael Gråborg <mikael.graborg@iteam.se>
2019-05-15 20:40:09 +0200

    fix: saveCV.tsx

--- src/views/CreateProfile/SaveCV.tsx
+++ src/views/CreateProfile/SaveCV.tsx
@@ -9,12 +9,12 @@ import { navigate } from '@reach/router'
 import { ConsentApprovedSubscription } from '../../generated/myskills'
 import styled from '@emotion/styled'
 import { OpenInApp } from '../../components/ButtonLink'
+import Loader from '../../components/Loader'
 
 export const GET_CONSENT_ID = gql`
-  mutation consent {
+  query consent {
     consent {
       id
-      expires
       url
     }
   }
@@ -27,32 +27,21 @@ export const SAVE_CV = gql`
     $experiences: [ExperienceInput!]
   ) {
     saveCV(
-      cv: { skills: $skills, education: $educations, experience: $experiences }
+      cv: {
+        skills: $skills
+        educations: $educations
+        experiences: $experiences
+        occupation: $occupation
+      }
     ) {
       skills {
         sourceId
-        term
-        type
-      }
-      education {
-        id
-        programme
-        school
-        start
-        end
-      }
-      experience {
-        sourceId
-        term
-        employer
-        start
-        end
       }
     }
   }
 `
 
-export const GET_LOCAL_CV = gql`
+export const GET_CV_CLIENT = gql`
   query getCV {
     skills @client {
       sourceId
@@ -76,6 +65,7 @@ export const GET_LOCAL_CV = gql`
         years
       }
     }
+    language
   }
 `
 
@@ -101,22 +91,18 @@ const QrWrapper = styled.div`
 `
 
 const Register: React.FC<RouteComponentProps> = props => {
-  const registerMutation = useMutation(GET_CONSENT_ID)
-  const saveCVMutation = useMutation(SAVE_CV)
-  const { data: localCV, loading, error } = useQuery(GET_LOCAL_CV)
-  const [consent, setConsent] = useState(null)
+  const {
+    data: { consent },
+    loading,
+    error,
+  } = useQuery(GET_CONSENT_ID)
 
-  useEffect(() => {
-    registerMutation().then(({ data: { consent } }) => {
-      setConsent(consent)
-    })
-  }, [])
+  const saveCVMutation = useMutation(SAVE_CV)
+  const { data: localCV } = useQuery(GET_CV_CLIENT)
 
   const onConsentApproved = async ({
     consentApproved,
   }: ConsentApprovedSubscription) => {
-    console.log('consentapprove', consentApproved)
-    console.log('data', localCV)
     setCookie('token', consentApproved.accessToken)
     await saveCVMutation({
       variables: {
@@ -128,17 +114,18 @@ const Register: React.FC<RouteComponentProps> = props => {
         })),
       },
     })
-    navigate('/profile')
+    navigate('/skapa-cv/congratulations')
   }
-
+  if (error) return <div>{error.message}</div>
+  if (loading) return <Loader />
   return (
     <Background>
       {consent && (
         <>
           <QrWrapper>
             <Consent
-              onConsentApproved={onConsentApproved}
               consentId={consent.id}
+              onConsentApproved={onConsentApproved}
               url={consent.url}
             />
           </QrWrapper>

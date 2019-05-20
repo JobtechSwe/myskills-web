2ea087aa87eaeaaf598118e9fea0f081ae75c1d4
Nils Nilsson <nilspontusnilsson@gmail.com>
2019-05-20 14:24:39 +0200

    feat: added edit mode on education form

--- src/components/AddedEducations.tsx
+++ src/components/AddedEducations.tsx
@@ -1,41 +1,39 @@
 import React from 'react'
-import { useQuery, useMutation } from 'react-apollo-hooks'
+import { useQuery } from 'react-apollo-hooks'
 import { GET_EDUCATIONS_CLIENT } from '../graphql/resolvers/mutations/addEducation'
 import { Education } from '../generated/myskills'
 import Timeline from './Timeline'
-import gql from 'graphql-tag'
 
-export const REMOVE_EDUCATIONS_CLIENT = gql`
-  mutation removeEducationClient($education: EducationInput!) {
-    removeEducationClient(education: $education) @client {
-      programme
-      school
-      start
-      end
-    }
-  }
-`
+interface AddedEducationsProps {
+  editingEntry?: string
+  handleEdit?: (entry: any) => void
+}
 
-const AddedEducations: React.FC = () => {
+const AddedEducations: React.FC<AddedEducationsProps> = ({
+  editingEntry,
+  handleEdit,
+}) => {
   const { data, loading, error } = useQuery(GET_EDUCATIONS_CLIENT)
-  /* const removeEducationClient = useMutation(REMOVE_EDUCATIONS_CLIENT) */
 
   if (error) return <div>{error.message}</div>
   if (loading) return <div>Loading...</div>
 
   return (
-    <div>
-      {data && data.experiences && (
+    <>
+      {data && data.educations && (
         <Timeline
+          editingEntry={editingEntry}
           entries={data.educations.map((education: Education) => ({
+            id: education.id,
             title: education.programme,
             schoolOrCompany: education.school,
             start: education.start,
             end: education.end,
           }))}
+          handleEdit={handleEdit}
         />
       )}
-    </div>
+    </>
   )
 }
 
--- src/views/CreateProfile/AddEducation.tsx
+++ src/views/CreateProfile/AddEducation.tsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useMutation } from 'react-apollo-hooks'
 import gql from 'graphql-tag'
 import { RouteComponentProps } from '@reach/router'
-import AddAndEditForm, { FormState } from '../../components/AddAndEditForm'
+import AddAndEditForm from '../../components/AddAndEditForm'
 import AddedEducations from '../../components/AddedEducations'
 import IllustrationHeader from '../../components/IllustrationHeader'
 import bookIllustration from '../../assets/illustrations/book.svg'
 import RegistrationLayout from '../../components/Layout/RegistrationLayout'
-import { EducationInput } from '../../generated/myskills.d'
+import { v4 } from 'uuid'
+import { Entry } from '../../components/Timeline/index'
+
+export type EducationInput = {
+  id?: String
+  programme: String
+  school: String
+  start: String
+  end?: String
+}
 
 export const ADD_EDUCATION_CLIENT = gql`
   mutation addEducationClient($education: EducationInput!) {
     addEducationClient(education: $education) @client {
+      id
       programme
       school
       start
@@ -20,21 +30,66 @@ export const ADD_EDUCATION_CLIENT = gql`
   }
 `
 
-export const ADD_EDUCATION_API = gql`
-  mutation addEducationApi($education: EducationInput!) {
-    addEducation(education: $education) {
-      programme
-      end
-      school
-      start
+const REMOVE_EDUCATION_CLIENT = gql`
+  mutation removeEducationClient($education: EducationInput!) {
+    removeEducationClient(education: $education) @client {
+      id
+    }
+  }
+`
+
+const UPDATE_EDUCATION_CLIENT = gql`
+  mutation updateEducationClient($education: EducationInput!) {
+    updateEducationClient(education: $education) @client {
+      id
     }
   }
 `
 
 const AddEducation: React.FC<RouteComponentProps> = () => {
-  const addEducationClient = useMutation(ADD_EDUCATION_CLIENT)
+  const initialEditEntry = {
+    id: '',
+    title: '',
+    degree: '',
+    schoolOrCompany: '',
+    start: '',
+    end: '',
+  } as Entry
 
-  const handleSubmit = ({ title, start, end, schoolOrCompany }: FormState) => {
+  const addEducationClient = useMutation(ADD_EDUCATION_CLIENT)
+  const removeEducationClient = useMutation(REMOVE_EDUCATION_CLIENT)
+  const updateEducationClient = useMutation(UPDATE_EDUCATION_CLIENT)
+
+  const [edit, toggleEdit] = useState(false)
+  const [editEntry, setEditEntry] = useState(initialEditEntry)
+
+  const handleEdit = (entry: Entry) => {
+    setEditEntry(entry)
+    toggleEdit(true)
+  }
+
+  const abortEdit = () => {
+    setEditEntry(initialEditEntry)
+    toggleEdit(false)
+  }
+
+  const handleDelete = (entry: Entry) => {
+    removeEducationClient({
+      variables: {
+        education: {
+          id: entry.id,
+          programme: entry.title,
+          school: entry.schoolOrCompany,
+          start: entry.start,
+          end: entry.end,
+        },
+      },
+    })
+
+    toggleEdit(false)
+  }
+
+  const handleSubmit = ({ id, title, start, end, schoolOrCompany }: Entry) => {
     const education: EducationInput = {
       end,
       programme: title,
@@ -42,9 +97,26 @@ const AddEducation: React.FC<RouteComponentProps> = () => {
       start,
     }
 
+    if (edit) {
+      updateEducationClient({
+        variables: {
+          education: {
+            ...education,
+            id,
+          },
+        },
+      })
+
+      setEditEntry(initialEditEntry)
+      return toggleEdit(false)
+    }
+
     addEducationClient({
       variables: {
-        education,
+        education: {
+          ...education,
+          id: v4(),
+        },
       },
     })
   }
@@ -57,13 +129,27 @@ const AddEducation: React.FC<RouteComponentProps> = () => {
         imageSource={bookIllustration}
         title="Vad har du för utbildning?"
       />
-      <AddedEducations />
-      <AddAndEditForm
-        label="Lägg till utbildning"
-        onSubmit={handleSubmit}
-        schoolOrCompanyPlaceholder="Namn på utbildning..."
-        titlePlaceholder="Namn på skola..."
-      />
+      <AddedEducations editingEntry={editEntry.id} handleEdit={handleEdit} />
+      {edit && (
+        <AddAndEditForm
+          abortEdit={abortEdit}
+          edit={true}
+          editItem={editEntry}
+          handleDelete={handleDelete}
+          label="Uppdatera utbildning"
+          onSubmit={handleSubmit}
+          schoolOrCompanyPlaceholder="Namn på utbildning..."
+          titlePlaceholder="Namn på skola..."
+        />
+      )}
+      {!edit && (
+        <AddAndEditForm
+          label="Lägg till utbildning"
+          onSubmit={handleSubmit}
+          schoolOrCompanyPlaceholder="Namn på utbildning..."
+          titlePlaceholder="Namn på skola..."
+        />
+      )}
     </RegistrationLayout>
   )
 }

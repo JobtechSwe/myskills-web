d4e3e37e1c308f3016682b56da4679f722190569
Tom Ekander <ekander.tom@gmail.com>
2019-05-20 09:26:37 +0200

    feat: implement add free-text traits (#55)
    * feat: add autocomplete to AddTrait
    
    * chore: linting

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,13 +1,37 @@
 import styled from '@emotion/styled'
-import { color, ColorProps, space, SpaceProps } from 'styled-system'
+import {
+  borders,
+  BordersProps,
+  borderRadius,
+  BorderRadiusProps,
+  color,
+  ColorProps,
+  space,
+  SpaceProps,
+} from 'styled-system'
 
-type ListProps = React.HTMLProps<HTMLUListElement> & ColorProps & SpaceProps
+type ListProps = React.HTMLProps<HTMLUListElement> &
+  BordersProps &
+  BorderRadiusProps &
+  ColorProps &
+  SpaceProps
 
-const List = styled.ul<ListProps>`
+export const List = styled.ul<ListProps>`
   list-style: none;
 
+  ${borders}
+  ${borderRadius}
   ${color}
   ${space}
+`
+
+const SearchList = styled(List)<{ isOpen: boolean }>`
+  border-bottom-left-radius: 8px;
+  border-bottom-right-radius: 8px;
+  box-shadow: ${({ theme }) => `0px 8px 16px 0px ${theme.colors.whiteLilac}`};
+  max-height: 224px;
+  overflow-x: hidden;
+  overflow-y: auto;
 `
 
 List.defaultProps = {
@@ -15,4 +39,10 @@ List.defaultProps = {
   p: 0,
 }
 
-export default List
+SearchList.defaultProps = {
+  borderTop: 0,
+  color: 'persianBlue',
+  p: 0,
+}
+
+export { List as default, SearchList }

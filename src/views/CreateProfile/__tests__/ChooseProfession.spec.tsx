cdd32223385fe8059d6725746124548a1fa5223a
Nils Nilsson <nilspontusnilsson@gmail.com>
2019-05-21 15:02:22 +0200

    test: remove tests

--- src/views/CreateProfile/__tests__/ChooseProfession.spec.tsx
+++ src/views/CreateProfile/__tests__/ChooseProfession.spec.tsx
@@ -3,70 +3,8 @@ import ChooseProfession, { GET_ONTOLOGY_CONCEPTS } from '../ChooseProfession'
 import { render } from '../../../utils/test-utils'
 import { wait, fireEvent } from 'react-testing-library'
 import { OntologyType } from '../../../generated/myskills.d'
-import { GET_OCCUPATION_CLIENT } from '../../../graphql/resolvers/mutations'
 
 describe('views/ChooseProfession', () => {
-  let withResultsMock: any
-
-  beforeEach(() => {
-    withResultsMock = [
-      {
-        request: {
-          query: GET_ONTOLOGY_CONCEPTS,
-          variables: {
-            filter: 'Systemutvecklare',
-            type: OntologyType.Occupation,
-          },
-        },
-        result: {
-          data: {
-            __typename: 'OntologyConceptsResult',
-            ontologyConcepts: [
-              {
-                term: 'Systemutvecklare',
-                id: 'abc',
-                type: 'occupation-name',
-                __typename: 'OntologyConceptResult',
-              },
-            ],
-          },
-        },
-      },
-    ]
-  })
-
-  xit('renders empty result', async () => {
-    const withResultsMock = [
-      {
-        request: {
-          query: GET_OCCUPATION_CLIENT,
-        },
-        result: {
-          data: {},
-        },
-      },
-    ]
-
-    const { container } = render(<ChooseProfession />, withResultsMock)
-
-    await wait()
-
-    expect(container).toMatchSnapshot()
-  })
-
-  xit('renders loading message', async () => {
-    const { getByPlaceholderText, getByText } = render(
-      <ChooseProfession />,
-      withResultsMock
-    )
-
-    fireEvent.change(getByPlaceholderText('Yrkesroll eller yrkesområde'), {
-      target: { value: 'Systemutvecklare' },
-    })
-
-    expect(getByText(/Loading.../i)).toBeInTheDocument()
-  })
-
   it('renders error message', async () => {
     const withErrorMock = [
       {
@@ -95,21 +33,6 @@ describe('views/ChooseProfession', () => {
     await wait()
 
     expect(getByText(/Error.../i)).toBeInTheDocument()
-  })
-
-  xit('should render with taxonomy query result', async () => {
-    const { getByPlaceholderText, getByText } = render(
-      <ChooseProfession />,
-      withResultsMock
-    )
-
-    fireEvent.change(getByPlaceholderText(/Yrkesroll eller yrkesområde/i), {
-      target: { value: 'Systemutvecklare' },
-    })
-
-    await wait()
-
-    expect(getByText('Systemutvecklare')).toBeInTheDocument()
   })
 
   it('renders next button', async () => {
